74cf1fb39372323b845721c180553d51e42a559b
Weilun Wang <owwlo@163.com>
2016-07-02 20:15:23 -0400

    update example.py to have the new features

--- example.py
+++ example.py
@@ -8,37 +8,37 @@ import sys
 import os
 import traceback
 
-from PyTerminalCommander import Commander, CommandHandler
+from PyTerminalCommander import Commander, CommandHandler, CommanderPopupLauncher
 
 if __name__=='__main__':
-
-    # Print things from another thread
-    import time
-    def run():
-        while True:
-            time.sleep(1)
-            print('Tick')
-    t = Thread(target = run)
-    t.daemon=True
-    t.start()
-
     # Define the handler
     class CommandSet(CommandHandler):
         def do_echo(self, commander, extra):
-            '''echo - Just echos all arguments'''
+            '''echo - Echo all arguments'''
             return extra
 
         def do_raise(self, commander, extra):
+            '''Raise an exception'''
             raise Exception('Some Error')
 
         def do_quit(self, *args):
+            '''Exit the program.'''
             return Commander.Exit
 
         def do_yellow(self, commander, extra):
+            '''Output the following text in yellow.'''
             return (extra, "yellow")
 
         def do_green(self, commander, extra):
+            '''Output the following text in green.'''
             return (extra, "light_green")
+
+        def do_notify(self, commander, extra):
+            '''Show a notification popup.'''
+            commander.notify(
+                ["This is a ", ("yellow", "notification: "), extra, "."],
+                CommanderPopupLauncher.DEFAULT_DURATION
+            )
 
     c = Commander('This is the title of the frame'
         , cmd_cb = CommandSet()
@@ -48,6 +48,18 @@ if __name__=='__main__':
         , show_line_num = True
         , extra_pallete = [
             ('light_green', urwid.LIGHT_GREEN, urwid.BLACK),
+            ('yellow', urwid.YELLOW, urwid.DARK_BLUE),
         ]
     )
+
+    # Print things from another thread
+    import time
+    def run():
+        while True:
+            time.sleep(1)
+            c.output('Tick')
+    t = Thread(target = run)
+    t.daemon=True
+    t.start()
+
     c.loop()
